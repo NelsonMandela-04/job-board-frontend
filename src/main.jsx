4df1de398858572f707a1349fff26e5fbeaba374
Ezitech042 <okorietukwasichukwuobi042@gmail.com>
2026-09-11 18:50:58 -0700

    Fix Vercel frontend build

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import App from "./App";
 
 import "./index.css";
 
+console.log("VITE API URL:", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
